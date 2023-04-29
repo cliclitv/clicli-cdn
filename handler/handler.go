9a9acbccff10c53a95f9a4f20af78575d95cefc0
unknown <1533540012@qq.com>
2023-04-29 12:55:33 +0800

    日常提交

--- handler/handler.go
+++ handler/handler.go
@@ -1,19 +1,20 @@
 package handler
 
 import (
+	"bytes"
 	"crypto/md5"
 	_ "embed"
 	"encoding/hex"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
-	"time"
 	"os"
-	"path"
-	"fmt"
-	"path/filepath"
 	"os/exec"
+	"path"
+	"path/filepath"
 	"strings"
+	"time"
 )
 
 //go:embed index.html
@@ -30,7 +31,7 @@ func Uplaod(w http.ResponseWriter, r *http.Request) {
 		}
 		file, head, err := r.FormFile("file")
 		ext := path.Ext(head.Filename)
-		if ext != ".mp4" && ext != ".mov" && ext != ".mkv"{
+		if ext != ".mp4" && ext != ".mov" && ext != ".mkv" {
 			sendMsg(w, 403, "暂时只支持mp4.h264文件")
 			return
 		}
@@ -46,9 +47,9 @@ func Uplaod(w http.ResponseWriter, r *http.Request) {
 		hexText := make([]byte, 32)
 		hex.Encode(hexText, cipherText2)
 
-		folderPath:=createDateDir("")
+		folderPath := createDateDir("")
 
-		name := folderPath+"/"+uid + "_" + string(hexText) + path.Ext(head.Filename)
+		name := folderPath + "/" + uid + "_" + string(hexText) + path.Ext(head.Filename)
 
 		newFile, err := os.Create(name)
 		if err != nil {
@@ -67,22 +68,25 @@ func Uplaod(w http.ResponseWriter, r *http.Request) {
 
 		go Transform(name)
 
-
 		sendMsg(w, 200, name)
 	}
 }
 
-func Transform(name string){
-		// 设置 ffmpeg 命令行参数
-		dir:= strings.Replace(name,".mp4","/",1)
-		i:="./"+dir+"index.m3u8"
-		o:="./"+dir+"out%03d.ts"
-		createDateDir(dir)
-//libfdk_aac
-		args := []string{"-i",
-		"./"+name,
-		"-c:v", "libx264", "-b:v" ,"2000k","-c:a","copy","-s","1920x1080",
-		"-vf", `movie=logo.png, scale=iw*0.4:-1 [logo]; [in][logo] overlay=10:10, scale=1920:-2,pad=iw:1080:0:(oh-ih)/2:black [out]`,
+func Transform(name string) {
+	// 设置 ffmpeg 命令行参数
+	dir := strings.Replace(name, ".mp4", "/", 1)
+	i := "./" + dir + "index.m3u8"
+	o := "./" + dir + "out%03d.ts"
+	createDateDir(dir)
+
+	//libfdk_aac
+	args := []string{
+		"-i",
+		"./" + name,
+		"-i", "logo.png",
+		"-filter_complex", `[1][0]scale2ref=iw/10:-2[b][a];[a][b] overlay=10:10,scale=1920:-2,pad=iw:1080:0:(oh-ih)/2:black`,
+		"-c:v", "libx264", "-b:v", "2000k", "-c:a", "copy",
+		// "-vf", `movie=logo.png, scale=` + output + `*0.4:-1 [logo]; [in][logo] overlay=10:10, scale=1920:-2,pad=iw:1080:0:(oh-ih)/2:black [out]`,
 		"-map",
 		"0",
 		"-f",
@@ -93,44 +97,39 @@ func Transform(name string){
 		"3",
 		o}
 
-		// 创建 *exec.Cmd
-		cmd := exec.Command("ffmpeg", args...)
+	// 创建 *exec.Cmd
+	cmd := exec.Command("ffmpeg", args...)
 
+	fmt.Println(cmd)
 
-		fmt.Println(cmd)
+	// 运行 ffmpeg 命令
+	if err := cmd.Run(); err != nil {
+		fmt.Println(err)
+		return
+	}
 
-		// output, _ := cmd.CombinedOutput()
-		// fmt.Println(string)
-	
-		// 运行 ffmpeg 命令
-		if err := cmd.Run(); err != nil {
-			fmt.Println(err)
-			return
-		}
+	// 最后删除文件
 
-		// 最后删除文件
-
-		_=os.Remove(name)
+	_ = os.Remove(name)
 }
 
 // https://github.com/cliclitv/clicli-cdn.git
 
-func createDateDir(path string)string{
+func createDateDir(path string) string {
 	var folderName string
-	if path == ""{
-		folderName=time.Now().Format("20060102")
-	}else{
+	if path == "" {
+		folderName = time.Now().Format("20060102")
+	} else {
 		folderName = path
 	}
-	
+
 	folderPath := filepath.Join("./", folderName)
 
-	
-	_,err:= os.Stat(folderPath); 
-	
-	if os.IsNotExist(err){
-		os.Mkdir(folderPath,0777)
-		os.Chmod(folderPath,0777)
+	_, err := os.Stat(folderPath)
+
+	if os.IsNotExist(err) {
+		os.Mkdir(folderPath, 0777)
+		os.Chmod(folderPath, 0777)
 	}
 	return folderPath
 }
@@ -143,4 +142,18 @@ func sendMsg(w http.ResponseWriter, code int, msg string) {
 	}{Code: code, Msg: msg})
 
 	io.WriteString(w, string(resStr))
+}
+
+func Cmd(commandName string, params []string) (string, error) {
+	cmd := exec.Command(commandName, params...)
+	fmt.Println("Cmd", cmd.Args)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = os.Stderr
+	err := cmd.Start()
+	if err != nil {
+		return "", err
+	}
+	err = cmd.Wait()
+	return out.String(), err
 }
